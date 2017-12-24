2b7bba704f4a3b3b4a4b5037d74889d4d0c4ffaa
Karim Kidiss <kkidiss@yahoo.fr>
2017-12-24 10:13:52 +0000

    Adds Player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Rigidbody2D myRB;
+    SpriteRenderer mySR;
+
+    public float maxSpeed;
+
+    bool facingRight = true;
+
 	// Use this for initialization
 	void Start () {
-		
+        myRB = GetComponent<Rigidbody2D>();
+        mySR = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float move = Input.GetAxis("Horizontal");
+        Debug.Log(move);
+
+        if(move > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            flip();
+        }
+
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
 	}
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        mySR.flipX = !mySR.flipX;
+    }
 }
